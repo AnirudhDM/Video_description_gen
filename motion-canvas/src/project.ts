8d54ae03fdb16a500f53c6abd9f0bcad0f86b36d
Anirudh Durai Mohan <anirudh.durai.mohan@Mac-0665.home>
2026-03-05 17:03:39 +0100

    motion_canvas

--- motion-canvas/src/project.ts
+++ motion-canvas/src/project.ts
@@ -1,7 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
-import scene from './scenes/same_endpoints_subarray_max_sum?scene';
+import scene from './scenes/garden_tree_equalization?scene';
 
 export default makeProject({
   scenes: [scene],
-  audio: "/Users/anirudh.durai.mohan/Documents/side pieces/hack/poc/audio/same_endpoints_subarray_max_sum_full.wav",
+  audio: "/Users/anirudh.durai.mohan/Documents/side pieces/hack/poc/audio/garden_tree_equalization_full.wav",
 });
